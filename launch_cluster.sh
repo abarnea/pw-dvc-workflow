5822b4e97efab36ddd74b30e8d9458601ec4e64d
abarnea <109750339+abarnea@users.noreply.github.com>
2023-07-28 11:56:50 -0500

    Change resource name variable again

--- launch_cluster.sh
+++ launch_cluster.sh
@@ -13,10 +13,10 @@ fi
 
 # Installs miniconda3 on remote machine with necessary packages from the
 # install_python.sh script
-bash python_installation/install_python.sh ${cluster_name}
+bash python_installation/install_python.sh ${resource_name}
 
 # Sends storage bucket path environment file to remote machine
-scp $( pwd )/storage_bucket.env ${hostname}@${cluster_name}.clusters.pw:/home/${USER}
+scp $( pwd )/storage_bucket.env ${hostname}@${resource_name}.clusters.pw:/home/${USER}
 
 # Starts the remote_dvc_setup script on the remote machine
-ssh ${cluster_name}.clusters.pw ${hostname} bash -s < remote_dvc_setup.sh ${repo_name}
+ssh ${resource_name}.clusters.pw ${hostname} bash -s < remote_dvc_setup.sh ${repo_name}
